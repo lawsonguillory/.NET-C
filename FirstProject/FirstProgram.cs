9b17342af3a25bae80484015a8a79873e7e5d171
lawsonguillory <77700731+lawsonguillory@users.noreply.github.com>
2021-01-22 00:21:58 -0600

    Add files via upload

---
+++ FirstProject/FirstProgram.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace FirstProject
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+
+         int ageInt;
+
+
+         System.Console.WriteLine("What is your name?");
+
+         string userName = Console.ReadLine();
+
+
+         System.Console.WriteLine("Hello "+userName+"!");
+
+         System.Console.WriteLine("What is your age?");
+
+         string age = Console.ReadLine();
+
+         ageInt=int.Parse(age);
+
+         Console.WriteLine("In 15 years, your age will be "+(ageInt+15));
+
+
+      }
+   }
+}
+
