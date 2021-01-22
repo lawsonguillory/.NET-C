384e47821e26a92896d0dae22700c645a80f02c8
lawsonguillory <77700731+lawsonguillory@users.noreply.github.com>
2021-01-22 00:36:35 -0600

    Update FirstProgram.cs
    Added additional commenting to explain Parse Function

--- FirstProject/FirstProgram.cs
+++ FirstProject/FirstProgram.cs
@@ -6,21 +6,30 @@ namespace FirstProject
    {
       static void Main(string[] args)
       {
-
+  
+         /// declares "ageInt" as int variable ///
+         
+         
          int ageInt;
 
-
+         
+         
          System.Console.WriteLine("What is your name?");
 
          string userName = Console.ReadLine();
 
 
+         
          System.Console.WriteLine("Hello "+userName+"!");
 
          System.Console.WriteLine("What is your age?");
 
          string age = Console.ReadLine();
-
+         
+         
+         /// Parses variable "age" into an integer to be added with variable "ageInt"
+         
+         
          ageInt=int.Parse(age);
 
          Console.WriteLine("In 15 years, your age will be "+(ageInt+15));
