e53f95a55b40917b07fc8cca0e7956fdc76ff3f9
lawsonguillory <77700731+lawsonguillory@users.noreply.github.com>
2021-01-25 21:24:35 -0600

    Add files via upload

---
+++ Week 2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace EmployeeInfo
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		// Declaration of Variables// 
+
+			string empname;
+			string birthdate;
+			string extensionname;
+			string email;
+
+			Console.WriteLine("Please Enter Name");
+
+			empname = Console.ReadLine();
+			Console.WriteLine("Enter Birthdate");
+
+			birthdate=Console.ReadLine();
+			Console.WriteLine("Enter Extension");
+	
+			extensionname=Console.ReadLine();
+			Console.WriteLine("Enter Email Address");
+
+			email=Console.ReadLine();
+
+			Console.WriteLine("Your Information is as Follows: ");
+
+																	// Printing Given Employee Information // 
+
+			Console.WriteLine(empname+ " " + birthdate + " " + extensionname + " " +email);
+
+
+
+
+		}
+	}
+}
